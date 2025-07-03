e8ff7c7487f4388771ca1a83e6849f2d74c5a014
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-07-03 04:43:15 -0700

    Update data_types.js

--- 180 days challenge/Day 12/data_types.js
+++ 180 days challenge/Day 12/data_types.js
@@ -1,10 +1,8 @@
 
-
-// var => global scope   can be reassigned and access from any where 
-// let => block scope   can be reassigned 
-//const => block scope, cannot be reassigned
-
-
+//  Variable declaration
+// var => global scope   can be reassigned and access from any where hoist  , can be redeclared
+// let => block scope   can be reassigned also hoist but after initialization , canot be redeclared but reassighn(update)
+//const => block scope, cannot be reassigned  also hoist but after initialization , cannot be redeclared
 
 
 // primitive data types
@@ -12,20 +10,20 @@
 // resource so we have strings , numbers , booleans ,
 // undefined , symbol 
 
-const name ="Areeba";
-const age = 25
-const isStudent = true;
+const name ="Areeba";   //strings
+const age = 25     //number
+const isStudent = true;   //boolean
 const yy = undefined; // undefined means no value assigned
  const city = null;  // null means empty or no value
-const rating = 5.6;
- console.log(typeof rating);
-
+const rating = 5.6;     //number
+ console.log(typeof rating);    //typeof is an operator used to check dataa type
+   
 
 
 //  concatinating strings
 console.log("My name is " + name + " and I am " + age);
 // Template strings
-const hello= `My name is ${name} and I am ${age}`
+const hello= My name is ${name} and I am ${age}
 
 console.log(hello);
 
@@ -51,7 +49,7 @@ console.time(10)
 const numbers = new Array(1,2,3,4,5);
 console.log(numbers)
 
-
+// some arrays properties
 const fruits = ["apples" , "oranges" , "pears: , 10 , true"]
 fruits[1] = "grapes";
 fruits.push("mangoes")
@@ -83,7 +81,34 @@ console.log(person.firstName , person.lastName);
 // destructuring object
 const {firstName , lastName , address:{city}} = person;
 
-console.log(firstName); 
+console.log(firstName);
 
 
 
+
+
+// objects in array
+const todos = [
+    {
+        id:1,
+        text:"Take out trash",
+        iscompleted: true,
+        
+    },
+
+    {
+        id : 2 ,
+        text:"Meeting with boss",
+        isCompleted : true,
+
+    },
+     {
+        id : 3 ,
+        text:"Dentist appt",
+        isCompleted : false,
+
+    }
+];
+
+const todoJson= JSON.stringify(todos);
+console.log(todoJson)
